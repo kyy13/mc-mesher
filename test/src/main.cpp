503b3c30592b2f7d224cdf200437f6fb026505d9
Kyy13 <kyy1393@gmail.com>
2022-02-01 00:18:21 -0500

    added gradient_noise library for testing.

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,7 +2,9 @@
 // Kyle J Burgess
 
 #include "MCmesher.h"
+#include "gradient_noise.h"
 
+#include <ctime>
 #include <vector>
 #include <iostream>
 #include <iomanip>
@@ -12,9 +14,9 @@ int main()
 {
     const Vector3<uint32_t> dataSize =
         {
-            .x = 256,
-            .y = 256,
-            .z = 256,
+            .x = 128,
+            .y = 128,
+            .z = 128,
         };
 
     const Vector3<uint32_t> meshOrigin =
@@ -31,17 +33,59 @@ int main()
             .z = dataSize.z - 1,
         };
 
+    std::cout << "generating noise...\n";
+    gnd::gradient_noise<float, 3> noise3d(532346);
+    std::vector<float> data(dataSize.x * dataSize.y * dataSize.z);
+    for (size_t z = 0; z != dataSize.z; ++z)
+    {
+        for (size_t y = 0; y != dataSize.y; ++y)
+        {
+            for (size_t x = 0; x != dataSize.x; ++x)
+            {
+                size_t i = x + dataSize.x * (y + dataSize.y * z);
+
+                data[i] = noise3d({
+                    static_cast<float>(x) * 0.05f,
+                    static_cast<float>(y) * 0.05f,
+                    static_cast<float>(z) * 0.05f,
+                });
+
+                if (data[i] < -1.0f) data[i] = -1.0f;
+                if (data[i] > 1.0f) data[i] = 1.0f;
+            }
+        }
+    }
+
+    std::cout << "generating meshes...\n";
+
     auto mesh = CreateMesh();
 
-    //GenerateMeshVN(mesh, data.data(), dataSize, meshOrigin, meshSize, 0.5f);
-
-    size_t size = CountIndices(mesh);
-    const uint32_t* arr = GetIndices(mesh);
-
-    for (size_t i = 0; i != size; ++i)
+    size_t n = 0;
+    clock_t dt = 0;
+    clock_t dt_sum = 0;
+    for (float isoLevel = -1.0f; isoLevel <= 1.0f; isoLevel += 0.05f)
     {
-        std::cout << arr[i] << "\n";
+        clock_t t = clock();
+        GenerateMeshFN(mesh, data.data(), dataSize, meshOrigin, meshSize, isoLevel);
+        dt = (clock() - t);
+        dt_sum += dt;
+
+        float tf = static_cast<float>(dt) / static_cast<float>(CLOCKS_PER_SEC);
+        std::cout
+            << "isoLevel=" << isoLevel
+            << ", dt=" << tf << "s"
+            << ", indices=" << CountIndices(mesh)
+            << ", vertices=" << CountVertices(mesh)
+            << '\n';
+
+        ++n;
     }
+
+    float tf_sum = static_cast<float>(dt_sum) / static_cast<float>(CLOCKS_PER_SEC);
+    std::cout
+        << "dt sum=" << tf_sum << "s"
+        << ", dt avg=" << (tf_sum / static_cast<float>(n))
+        << '\n';
 
     DeleteMesh(mesh);
 }
