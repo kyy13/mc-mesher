fa371eb2630652d5e4506c7b720d67a054e713e4
Kyy13 <kyy1393@gmail.com>
2022-03-23 20:44:14 -0400

    adding godot wrapper

---
+++ wrappers/godot/include/mc_mesher.h
@@ -0,0 +1,20 @@
+// mc-mesher
+// Kyle J Burgess
+
+#ifndef MCM_GODOT_WRAPPER
+#define MCM_GODOT_WRAPPER
+
+#include "godot.hpp"
+#include "object.hpp"
+
+namespace godot
+{
+    class McmMeshBuffer : public Object
+    {
+        GODOT_CLASS(McmMeshBuffer, Object);
+    public:
+
+    };
+}
+
+#endif
---
+++ wrappers/godot/src/mc_mesher.cpp
@@ -0,0 +1,9 @@
+// mc-mesher
+// Kyle J Burgess
+
+#include "mc_mesher.h"
+
+namespace godot
+{
+
+}
